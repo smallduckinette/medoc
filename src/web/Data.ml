532cac66880a08e1e78fd7e692aa80f6cd69ae43
small_duck <small_duck@technoturtle.net>
2011-11-06 17:45:52 +0000

    Added copyright and license information

--- src/web/Data.ml
+++ src/web/Data.ml
@@ -1,3 +1,19 @@
+(*
+  Copyright 2010 small_duck
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
 
 type document_t = int * string * CalendarLib.Date.t * int list
 
